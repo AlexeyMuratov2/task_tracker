a28bed12a12b0ac0bb25b633f225919454c48c95
MuratovAlexey <alexey.e.muratov@gmail.com>
2024-11-08 22:02:28 +0800

    done maps task

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Node {
+    private Task data;
+    private Node next;
+    private Node pref;
+
+    public Node(Task data){
+        this.data = data;
+        this.next = null;
+        this.pref = null;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public Node getPref() {
+        return pref;
+    }
+
+    public Task getData() {
+        return data;
+    }
+
+    public void setNext(Node next1){
+        this.next = next1;
+    }
+
+    public void setPref(Node pref1){
+        this.pref = pref1;
+    }
+}
