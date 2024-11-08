5f255ad1914cf6ffefbf226ca0f455ab1b121436
MuratovAlexey <alexey.e.muratov@gmail.com>
2024-11-08 22:02:28 +0800

    done maps task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,37 +7,32 @@ public class Main {
         TaskManager manager = Managers.getDefault();
         manager.createNewTask(new Task(0, "Wake up", "wake up at 6:00"));
         manager.createNewTask(new Epic(1, "cook breakfast", "cook scramble eggs"));
-        manager.createNewTask(new Subtask(2, "cook it", manager.getByID(1)));
-        manager.createNewTask(new Epic(3, "go to work"));
-        manager.createNewTask(new Subtask(4, "get a shower", manager.getByID(3)));
-        manager.createNewTask(new Subtask(5, "dress up", manager.getByID(3)));
+        manager.createNewTask(new Epic(2, "go to work"));
+        manager.createNewTask(new Subtask(3, "get a shower", manager.getByID(2)));
+        manager.createNewTask(new Subtask(4, "eat", manager.getByID(2)));
+        manager.createNewTask(new Subtask(5, "dress up", manager.getByID(2)));
 
 
-        manager.updateStatus(manager.getByID(0), Status.IN_PROGRESS);
-        manager.updateStatus(manager.getByID(2), Status.IN_PROGRESS);
-        manager.updateStatus(manager.getByID(4), Status.DONE);
-        manager.updateStatus(manager.getByID(5), Status.IN_PROGRESS);
-
-        for (Task task : manager.getTaskData().values()) {
-            System.out.println(task.getName() + " " + task.getStatus());
-        }
-        for (Task task : manager.getEpicData().values()) {
-            System.out.println(task.getName() + " " + task.getStatus());
-        }
-        for (Task task : manager.getSubtaskData().values()) {
-            System.out.println(task.getName() + " " + task.getStatus());
-        }
-
-        System.out.println("  ");
-        manager.getByID(0);
         manager.getByID(1);
-        manager.getByID(2);
-        manager.getByID(3);
-        manager.getByID(4);
-        manager.getByID(5);
         List<Task> history = manager.getTaskHistory();
         for (int i = history.size() - 1; i >= 0; i--) {
             System.out.println((history.size() - i) + ": " + history.get(i).getName());
         }
+        System.out.println("");
+        manager.getByID(1);
+        manager.getByID(2);
+        history = manager.getTaskHistory();
+        for (int i = history.size() - 1; i >= 0; i--) {
+            System.out.println((history.size() - i) + ": " + history.get(i).getName());
+        }
+        System.out.println("");
+        manager.getByID(3);
+        manager.getByID(5);
+        manager.getByID(5);
+        history = manager.getTaskHistory();
+        for (int i = history.size() - 1; i >= 0; i--) {
+            System.out.println((history.size() - i) + ": " + history.get(i).getName());
+        }
+        System.out.println("");
     }
 }
